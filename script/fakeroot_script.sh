182696613e6d6b20f2197144d79c8d42a7579a42
Dang Ngoc Lam <ngoclamdang26@gmail.com>
2024-09-02 21:32:42 +0700

    Work around for starting ntp too early , facing an error

--- script/fakeroot_script.sh
+++ script/fakeroot_script.sh
@@ -2,4 +2,5 @@
 echo "Executing script in fakeroot env"
 mkdir -p ${TARGET_DIR}/mnt/data
 mkdir -p ${TARGET_DIR}/oem/bin/
+mv ${TARGET_DIR}/etc/init.d/S49ntp ${TARGET_DIR}/etc/init.d/S99ntp
 chmod 755 ${TARGET_DIR}/etc/init.d/S01syslogd
